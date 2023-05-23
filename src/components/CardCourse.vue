1231eed8b607b8b24f38032344e03d8bc336c950
HenriqueMoroziniHupp <henriquehupp@hotmail.com>
2023-05-23 02:27:04 -0300

    news testing stiles on courseCard

--- src/components/CardCourse.vue
+++ src/components/CardCourse.vue
@@ -5,11 +5,7 @@
 <template>
   <div class="card-course">
     <div class="card-course__content">
-      <!-- <img class="course-icon" alt="Vue logo" src="@/components/icons/vue-badge.svg" /> -->
-      <div class="course-icon">
-        <img class="badge" alt="Vue logo" src="@/components/icons/badge.svg" height="125"/>
-        <img class="logo" alt="Vue logo" src="@/assets/logo.svg"  />
-      </div>
+      <img class="course-icon" alt="Vue logo" src="@/assets/icons/Pinia.webp"  />
       <div class="course-info">
         <h2 class="title">Front-end com Vue</h2>
         <p class="description">Aprenda o básico ao avançado com o poderoso Vue3</p>
@@ -20,21 +16,46 @@
         </div>
       </div>
     </div>
-    <div class="card-course__meta">
-      <!-- <div class="meta-card lesson">12</div> -->
+    <!-- <div class="card-course__meta">
+      <div class="meta-card lesson">12</div>
       <div class="meta-card duration">2h</div>
-    </div>
+    </div> -->
   </div>
+  <br>
+  <div class="card-course">
+    <div class="card-course__content">
+      <img class="course-icon" alt="Vue logo" src="@/assets/icons/Vue.webp"  />
+      <div class="course-info">
+        <h2 class="title">Front-end com Vue</h2>
+        <p class="description">Aprenda o básico ao avançado com o poderoso Vue3</p>
+      </div>
+      <div class="course-progress">
+        <div class="out-bar">
+          <div class="inner-bar" />
+        </div>
+      </div>
+    </div>
+    <!-- <div class="card-course__meta">
+      <div class="meta-card lesson">12</div>
+      <div class="meta-card duration">2h</div>
+    </div> -->
+  </div>
+
 </template>
 
 <style scoped lang="scss">
 .card-course {
   color: $white;
+  color: black;
   background-color: seagreen;
+  // background: -webkit-linear-gradient(315deg,#42d392 25%,#647eff);
+  // background: -webkit-linear-gradient(144deg, #A1CDCE 25%, #647eff);
+  // background: -webkit-linear-gradient(144deg, #A1CDCE 25%, #5566b7);
   display: grid;
   padding: 1.5rem;
   border-radius: $radius-lg;
   gap: 2rem;
+  transition: all 0.2s ease;
   
   @include screen(tablet-portrait-up) {
     grid: 1fr / 1fr auto;
@@ -45,6 +66,7 @@
     // background-color: blue;
   }
 
+
   &__content {
     display: grid;
     gap: 1rem;
@@ -52,19 +74,29 @@
     // grid: 1fr 1fr / 1fr 1fr;
     
     @include screen(tablet-portrait-up) {
+      grid: 90px auto / 90px auto;
       justify-content: start;
     }
 
     .course-icon {
+      height: 90px;
+      width: 100%;
       justify-self: center;
-
+      display: grid;
+      place-items: center;
+      object-fit: contain;
+      transition: all 0.2s ease;
     }
 
     .course-info {
       // grid-row: 1;
+      display: grid;
+      align-content: start;
+      gap: 0.5rem;
       .title {
         font-weight: $font-semi-bold;
         font-size: $font-semi-bold;
+        line-height: 1;
       }
 
       .description {
@@ -73,8 +105,6 @@
     }
 
     .course-progress {
-      // color: aquamarine;
-      // background-color: tomato;
       width: 100%;
       
       @include screen(tablet-portrait-up) {
@@ -114,4 +144,13 @@
       width: 4rem;
     }
   }
-}</style>
+}
+
+.card-course:hover {
+  transform: scale(1.02);
+
+  .course-icon {
+    transform: scale(1.3);
+  }
+}
+</style>
