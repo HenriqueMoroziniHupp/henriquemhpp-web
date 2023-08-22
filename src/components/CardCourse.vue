10d05a72d66faa0532cb3debc3449dd516510282
HenriqueMoroziniHupp <henriquehupp@hotmail.com>
2023-08-22 00:45:28 -0300

    add styles, interfaces and ajdust layout

--- src/components/CardCourse.vue
+++ src/components/CardCourse.vue
@@ -1,100 +1,60 @@
 <script setup lang="ts">
 
+defineProps({
+  courseTitle: {
+    type: String,
+    required: true,
+  },
+  courseDescription: {
+    type: String,
+    required: true,
+  },
+  courseIcon: {
+    type: String,
+    required: true,
+  }
+})
+
 </script>
 
 <template>
-  <a href="/" class="card-course">
-    <div class="card-course__content">
-      <img class="course-icon" alt="Vue logo" src="@/assets/icons/Pinia-bw.png"  />
-      <div class="course-info">
-        <h2 class="title">Front-end com Vue</h2>
-        <p class="description">Aprenda o básico ao avançado com o poderoso Vue3</p>
-      </div>
-      <div class="course-progress">
-        <div class="out-bar">
-          <div class="inner-bar" />
-        </div>
-      </div>
-    </div>
-    <!-- <div class="card-course__meta">
-      <div class="meta-card lesson">12</div>
-      <div class="meta-card duration">2h</div>
-    </div> -->
-  </a>
-  <br>
   <div class="card-course">
     <div class="card-course__content">
-      <img class="course-icon" alt="Vue logo" src="@/assets/icons/Vue.webp"  />
+      <img class="course-icon" alt="Vue logo" :src="courseIcon"  />
       <div class="course-info">
-        <h2 class="title">Front-end com Vue</h2>
-        <p class="description">Aprenda o básico ao avançado com o poderoso Vue3</p>
+        <h2 class="title">{{ courseTitle}}</h2>
+        <p class="description">{{ courseDescription }}</p>
       </div>
-      <!-- <div class="course-progress">
-        <div class="out-bar">
-          <div class="inner-bar" />
-        </div>
-      </div> -->
     </div>
-    <!-- <div class="card-course__meta">
-      <div class="meta-card lesson">12</div>
-      <div class="meta-card duration">2h</div>
-    </div> -->
   </div>
-
 </template>
 
 <style scoped lang="scss">
 .card-course {
-  // color: $white;
-  color: black;
-  // background-color: $soft-green;
+  color: $soft-gray-500;
   background-color: rgba($soft-green, 0.6);
-  // background-color: seagreen;
-  // background: -webkit-linear-gradient(144deg, #A1CDCE 25%, #5566b7);
   display: grid;
   padding: 1.5rem;
   border-radius: $radius-md;
   gap: 2rem;
   transition: all 0.2s ease;
-  
-  // @include screen(tablet-portrait-up) {
-  //   grid: 1fr / 1fr auto;
-  // }
-  
-
-  @include screen(tiny-phone-up) {
-    // background-color: blue;
-  }
 
 
   &__content {
     display: grid;
     gap: 1rem;
-    // justify-items: center;
-    // grid: 1fr 1fr / 1fr 1fr;
     
     @include screen(tablet-portrait-up) {
-      grid: 90px auto / 90px auto;
+      grid: auto / 90px auto;
       justify-content: start;
-    }
-
-    @include screen(phone-only) {
-      .icon-wrapper {
-        background-color: $soft-green;
-        padding: 0.8rem;
-        border-radius: $radius-sm;
-        display: grid;
-        place-items: center;
-        height: 140px;
-      }
     }
 
     .course-icon {
       height: 90px;
       width: 100%;
-      justify-self: center;
       display: grid;
       place-items: center;
+      place-self: center;
       object-fit: contain;
       transition: all 0.2s ease;
       aspect-ratio: 1 / 1;
@@ -139,24 +99,6 @@
       }
     }
 
-  }
-
-  &__meta {
-    display: grid;
-    align-content: start;
-    // gap: 0.8rem;
-    
-    .meta-card {
-      color: $black;
-      font-size: $text-2xl;
-      text-align: center;
-      font-weight: $font-semi-bold;
-      background-color: $white;
-      border-radius: $radius-normal;
-      padding: 1rem;
-      height: 4rem;
-      width: 4rem;
-    }
   }
 }
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,72 @@
 <script setup lang="ts">
 import CardCourse from '@/components/CardCourse.vue';
+import type { ICourse } from '@/api/interfaces/courses'
+
+
+const courses: Array<ICourse> = [
+  {
+    title: 'Front-end com Vue',
+    description: 'Aprenda o básico ao avançado com o poderoso Vue3',
+    icon: 'src/assets/icons/Vue.webp'
+  },
+  {
+    title: 'Estado global com Pinia',
+    description: 'Store moderna com compositions API, seguindo padrões consistentes e boas práticas adotada pela comunidade',
+    icon: 'src/assets/icons/Pinia.webp'
+  },
+  {
+    title: 'Estado global com Pinia',
+    description: 'Store moderna com compositions API, seguindo padrões consistentes e boas práticas adotada pela comunidade',
+    icon: 'src/assets/icons/Pinia.webp'
+  },
+  {
+    title: 'JavaScript ES6+',
+    description: 'Aprenda JS de forma moderna, com recursos de 2022',
+    icon: 'src/assets/icons/javascript.webp',
+  },
+  {
+    title: 'JavaScript ES6+',
+    description: 'Aprenda JS de forma moderna, com recursos de 2022',
+    icon: 'src/assets/icons/javascript4.webp',
+  },
+  {
+    title: 'Front-end com Vue',
+    description: 'Aprenda o básico ao avançado com o poderoso Vue3',
+    icon: 'src/assets/icons/Vue.webp'
+  },
+  {
+    title: 'DefineProps',
+    description: 'Aprenda a utilizar props e tipa-las',
+    icon: 'src/assets/icons/Vue.webp',
+  }
+]
 </script>
 
 <template>
   <main>
-    <CardCourse/>
+    <h1>Heui hue teste</h1>
+    <div class="course-container">
+      <CardCourse
+      v-for="course in courses"
+      :key="course.title"
+      :course-title="course.title"
+      :course-description="course.description"
+      :course-icon="course.icon"
+      class="card-course"
+    />
+    </div>
   </main>
 </template>
+
+<style scoped lang="scss">
+
+.course-container {
+  // margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr;
+  @include screen(tablet-landscape-up) {
+    grid-template-columns: 1fr 1fr;
+  }
+  gap: 1.2rem;
+}
+</style>
